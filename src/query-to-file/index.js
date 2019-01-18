4e8706fd4191b4fe88646bb42256a8955e77c30c
Krisztian Toth <ktoth2@emarsys.com>
2019-01-18 13:04:21 +0100

    feat(query-to-file): filename comes from a parameter PKG-0023
    Co-authored-by: Tamas Mohos <zerosuxx@gmail.com>

--- src/query-to-file/index.js
+++ src/query-to-file/index.js
@@ -8,21 +8,22 @@ const BigQuery = require('../big-query');
 
 class QueryToFile {
 
-  static create(baseName, formatOptions = { format: 'JSON', gzip: true }) {
+  static create(baseName, formatOptions = { format: 'JSON', gzip: true }, filename = null) {
     const tableName = `tmp_${baseName}_${+new Date}`;
 
-    return new QueryToFile(tableName, formatOptions);
+    return new QueryToFile(tableName, formatOptions, filename);
   }
 
 
-  constructor(tableName, formatOptions = { format: 'JSON', gzip: true }) {
+  constructor(tableName, formatOptions = { format: 'JSON', gzip: true }, filename = null) {
     this._tableName = tableName;
     this._formatOptions = formatOptions;
+    this._fileName = filename || `tmp/${this._tableName}.json.gz`;
   }
 
 
   *run(query, options) {
-    const file = File.create(`tmp/${this._tableName}.json.gz`);
+    const file = File.create(this._fileName);
 
     yield QueryToTable.create(this._tableName).run(query, options);
     yield TableToFile.create(this._tableName, file, this._formatOptions).run();
